226ada590ae75cb4fde1fffc05c8b3783200e21b
Yonatan Rojas S <yonatanrojas09@gmail.com>
2018-10-14 18:22:29 -0500

    added store to app

--- src/entries/home.js
+++ src/entries/home.js
@@ -3,7 +3,21 @@ import { render } from "react-dom";
 import Home from '../pages/containers/home';
 // import Playlist from '../playlist/components/playlist';
 import data from '../api.json';
+import { createStore } from 'redux';
 
+const initialState = {
+  data: {
+    ...data
+  }
+}
+
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+console.log(store.getState());
 
 const homeContainer = document.getElementById('home-container');
 
